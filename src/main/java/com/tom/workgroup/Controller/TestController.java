b6ab885783d741f7f6fc8845316cc1296bbffc97
tom <an__chen@163.com>
2017-05-28 02:49:16 +0800

    master push to github

---
+++ src/main/java/com/tom/workgroup/Controller/TestController.java
@@ -0,0 +1,14 @@
+package com.tom.workgroup.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/ok")
+    public String test(){
+        return  "ok";
+    }
+}
